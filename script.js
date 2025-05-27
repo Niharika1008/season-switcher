181d81a3b8cf1fa8f07cbf9bdcd0367f8e15df95
Niharika Chunchu <niharikachunchu10@gmail.com>
2025-05-27 17:09:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+let mediumImage = document.getElementById("seasonMediumImage");
+let smallImage = document.getElementById("seasonSmallImage");
+
+function springBtnClick() {
+    mediumImage.src =
+        "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-spring-md-img.png";
+    smallImage.src =
+        "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-spring-xs-img.png";
+
+}
+
+function summerBtnClick() {
+    mediumImage.src =
+        "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-summer-md-img.png";
+    smallImage.src =
+        "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-summer-xs-img.png";
+
+}
+
+function autumnBtnClick() {
+    mediumImage.src =
+        "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-autumn-md-img.png";
+    smallImage.src =
+        "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-autumn-xs-img.png";
+
+}
+
+function winterBtnClick() {
+    mediumImage.src =
+        "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-winter-md-img.png";
+    smallImage.src =
+        "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-winter-xs-img.png";
+
+}
